daf4dc92400a6e338cc2dd5b7269f55b3455c861
jamesthomaskerr <jamesthomaskerr@gmail.com>
2015-10-23 12:39:26 +0100

    Hangman
    Lab 9: everything working

---
+++ hangman.py
@@ -0,0 +1,69 @@
+__author__ = 'James'
+
+import random
+
+def genRandomWord():
+    list_of_words = ["python", "jumble", "easy", "difficult", "answer",  "xylophone"]
+    #word = random.choice(list_of_words)
+    word="difficult"
+    return word
+
+def createCurrentBlank(word):
+    currentBlanks = len(word) * "_"
+    return currentBlanks
+
+def printCurrentBlanks(currentBlanks):
+    for i in range(0,len(currentBlanks)):
+        print(currentBlanks[i],end=' ')
+
+
+def guessALetter():
+    letter=input("Guess a letter: ")
+    return letter
+
+def goodGuess(letter,word):
+    #returns true if letter is in word otherwise return false
+    match=False
+    for i in range(len(word)):
+        if word[i]==letter:
+            match=True
+
+    return match
+
+
+
+
+def updateCurrentBlanks(letter,word,currentBlanks):
+    # check word for letter and replace blanks in currentBlacks with any matches
+    wordlist=list(word)
+    for i in range(len(wordlist)):
+        if letter == wordlist[i]:
+            currentBlanks = currentBlanks[:i] + wordlist[i] + currentBlanks[i+1:]
+    return currentBlanks
+
+def printRules():
+    print('rules!')
+
+def printLives(currentLives):
+    print(currentLives)
+
+
+def main():
+    printRules()
+
+    word = genRandomWord()
+    currentBlanks = createCurrentBlank(word)
+    printCurrentBlanks(currentBlanks)
+    currentLives = 10
+    while currentLives > 0 and word != currentBlanks:
+        letter = guessALetter()
+        lastGuessGood = goodGuess(letter,word)
+        if lastGuessGood:
+            currentBlanks = updateCurrentBlanks(letter,word,currentBlanks)
+        else:
+            currentLives = currentLives - 1
+
+        printCurrentBlanks(currentBlanks)
+        printLives(currentLives)
+
+main()
